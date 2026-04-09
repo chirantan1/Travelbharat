1f0714a69c5fea1584695e35bfd02ce37a895d72
chirantan1 <chirantanjana43@.com>
2026-04-10 01:21:35 +0530

    3

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:8080',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
+  'https://travelbharat-frontend.onrender.com'
 ];
 
 app.use(cors({
